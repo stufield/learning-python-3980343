4158a48da52f649e37e85ae2ed66e31e16b6c41a
Stu Field <stu.g.field@gmail.com>
2025-05-14 17:15:48 -0600

    Complete loops start script example

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -1,19 +1,36 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with loops
 
-
 x = 0
 
 # define a while loop
+while x <= 5:
+  print(x)
+  x = x + 1
 
+#while answer != "yes":
+#  print(answer)
+#  answer = input("Stop? ")
 
 # define a for loop
-
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+for d in days:
+  print(d)
 
 # use a for loop over a collection
 
 
 # use the break and continue statements
+for d in days:
+  if (d == "Wed"):
+    break
+  print(d)
 
+for d in days:
+  if (d == "Wed"):
+    continue    # like 'next' in R
+  print(d)
 
 # using the enumerate() function to get an index and an item
+for i,d in enumerate(days):
+  print(i, d)
