badc29c9abe358fc050a40219e90475c28af0990
Stu Field <stu.g.field@gmail.com>
2025-05-14 17:05:43 -0600

    Complete conditional start script example

--- Start/Ch2/conditionals_start.py
+++ Start/Ch2/conditionals_start.py
@@ -1,9 +1,19 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for working with conditional statements
 
-
 x, y = 10, 100
 
 # conditional flow uses if, elif, else
+if x < y:
+  print("'x' is less than 'y'")
+
+if x < y:
+  print("'x' is less than 'y'")
+elif x == y:
+  print("'x' is the same as 'y'")
+else:
+  print("'x' is greater than 'y'")
 
 # conditional statements let you use "a if C else b"
+result = "'x' is less than 'y'" if (x < y) else "'x' is greater or equal to 'y'"
+print(result)
